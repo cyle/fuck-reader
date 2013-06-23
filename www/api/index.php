45da94d6edc41367b3ec54f2ee4ee2e45139cbf2
Cyle <cylegage@gmail.com>
2013-06-23 17:18:45 -0400

    - added dummy file for future API

---
+++ www/api/index.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+
+	functions to provide to an API
+	
+		- authenticate!
+			- give the person a long unique string as an auth token
+		- upload OPML file
+		- get posts from user's feeds
+		- add feed
+		- get posts
+		- mark post as read/unread
+		- mark all posts in feed as read
+		- mark absolutely all posts as read
+		- star/unstar post
+
+*/
+
+?>
