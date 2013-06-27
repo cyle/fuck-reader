dd79f99239cfaed02fabc1313525eef18cfb9b00
Cyle <cylegage@gmail.com>
2013-06-26 23:28:20 -0400

    - added blurb on subs page about google reader importing - feeds list on subs page does not show up if you have no feeds, lol

--- www/subs.php
+++ www/subs.php
@@ -21,34 +21,40 @@ require_once('head.php');
 	<?php if (isset($_GET['unread_success'])) { ?><p class="success">Marked selected feed(s) as unread.</p><?php } ?>
 	<?php if (isset($_GET['delete_success'])) { ?><p class="success">Deleted selected feed(s) from your subscriptions.</p><?php } ?>
 	
+	<p>If you have a Google Reader OPML subscriptions.xml file you'd like to import, head over to <a href="/settings/">/settings/</a></p>
+	
 	<form action="/subs/process/" method="post">
 	<p>Make sure you're adding the actual link to the site's <i>feed</i>, btw. Not just a link to a site you like.</p>
 	<p>This accepts standard RSS 1.0, 1.1, 2.0, and Atom feeds.</p>
 	<p>Add a new feed: <input type="hidden" name="a" value="n" /><input type="url" name="feed" /> <input type="submit" value="add &raquo;" /></p>
 	</form>
 	
-	<form action="/subs/process/" method="post">
-	<table>
-	<tr><th></th><th>Name</th><th>URL</th><th>Options</th></tr>
+	
 	<?php
 	
 	$get_feeds = $mysqli->query('SELECT * FROM feeds WHERE feed_id IN (SELECT feed_id FROM users_feeds WHERE user_id='.$current_user_id.') ORDER BY feed_title ASC, feed_id ASC');
 	
-	while ($feed = $get_feeds->fetch_assoc()) {
-		echo '<tr>';
-		echo '<td><input type="checkbox" value="'.$feed['feed_id'].'" name="feed-id[]" /></td>';
-		echo '<td>'.$feed['feed_title'].'</td>';
-		echo '<td>'.$feed['feed_homeurl'].'</td>';
-		echo '<td><a href="/subs/process/d/'.$feed['feed_id'].'/">delete</a> / <a href="/subs/process/u/'.$feed['feed_id'].'/">mark all unread</a></td>';
-		echo '</tr>'."\n";
-	}
-	
+	if ($get_feeds->num_rows > 0) {
+		?>
+	<form action="/subs/process/" method="post">
+	<table>
+	<tr><th></th><th>Name</th><th>URL</th><th>Options</th></tr>
+	<?php
+		while ($feed = $get_feeds->fetch_assoc()) {
+			echo '<tr>';
+			echo '<td><input type="checkbox" value="'.$feed['feed_id'].'" name="feed-id[]" /></td>';
+			echo '<td>'.$feed['feed_title'].'</td>';
+			echo '<td>'.$feed['feed_homeurl'].'</td>';
+			echo '<td><a href="/subs/process/d/'.$feed['feed_id'].'/">delete</a> / <a href="/subs/process/u/'.$feed['feed_id'].'/">mark all unread</a></td>';
+			echo '</tr>'."\n";
+		}
 	?>
 	</table>
-	
 	<p>With selected: <select name="a"><option value="0">Please select...</option><option value="d">Delete</option><option value="u">Mark all unread</option></select> <input type="submit" value="Do it &raquo;" /></p>
-	
 	</form>
+	<?php
+	} // end if you even have feeds
+	?>
 
 </div>
 <?php require_once('footer.php'); ?>
