23ea624345042956c2aa4985b97ef8fbccfd901f
Cyle <cylegage@gmail.com>
2013-06-26 22:24:57 -0400

    - added unique IDs of where unread counts are in the sidebar

--- www/sidebar.php
+++ www/sidebar.php
@@ -12,7 +12,7 @@ if (!isset($users_feeds) || $users_feeds == false || count($users_feeds) == 0) {
 } else {
 	?>
 	<ul>
-	<li><a href="/feeds/"<?php echo ((isset($just_unread) && $just_unread == true && !isset($selected_feed_id)) ? ' class="active"' : ''); ?>>Show All <?php echo getAllUnreadCount($current_user_id); ?> Unread Posts</a></li>
+	<li><a href="/feeds/"<?php echo ((isset($just_unread) && $just_unread == true && !isset($selected_feed_id)) ? ' class="active"' : ''); ?>>Show All <span id="unread-feeds-count"><?php echo getAllUnreadCount($current_user_id); ?></span> Unread Posts</a></li>
 	<li><a href="/feeds/all/"<?php echo ((isset($just_unread) && $just_unread == false && !isset($selected_feed_id)) ? ' class="active"': ''); ?>>Show All Posts, even Read</a></li>
 	<li><a href="/starred/">Show Only Starred Posts</a></li>
 	<li>Your Feeds:</li>
@@ -21,7 +21,8 @@ if (!isset($users_feeds) || $users_feeds == false || count($users_feeds) == 0) {
 	foreach ($users_feeds as $feed_id => $feed_title) {
 		$unread_count = getFeedUnreadCount($current_user_id, $feed_id);
 		echo '<li>';
-		echo '['.$unread_count.'] <a href="/feed/'.$feed_id.'/" '.((isset($selected_feed_id) && $feed_id == $selected_feed_id) ? 'class="active"': '').'>'.$feed_title.'</a> (<a href="/feed/'.$feed_id.'/all/" '.((isset($selected_feed_id) && $feed_id == $selected_feed_id && isset($just_unread) && $just_unread == false) ? 'class="active"': '').'>All Items</a>)';
+		//echo '['.$unread_count.'] <a href="/feed/'.$feed_id.'/" '.((isset($selected_feed_id) && $feed_id == $selected_feed_id) ? 'class="active"': '').'>'.$feed_title.'</a> (<a href="/feed/'.$feed_id.'/all/" '.((isset($selected_feed_id) && $feed_id == $selected_feed_id && isset($just_unread) && $just_unread == false) ? 'class="active"': '').'>All Items</a>)';
+		echo '['.((isset($selected_feed_id) && $feed_id == $selected_feed_id) ? '<span id="unread-feed-count">' : '').$unread_count.((isset($selected_feed_id) && $feed_id == $selected_feed_id) ? '</span>' : '').'] <a href="/feed/'.$feed_id.'/" '.((isset($selected_feed_id) && $feed_id == $selected_feed_id) ? 'class="active"': '').'>'.$feed_title.'</a>';
 		echo '</li>'."\n";
 	}
 	?>
