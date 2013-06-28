b7b4d4ae1ad5fb7c6fb7d81807417f314134689b
Cyle <cylegage@gmail.com>
2013-06-27 20:42:27 -0400

    - changed URLs to allow https

--- www/footer.php
+++ www/footer.php
@@ -1,5 +1,5 @@
 <div id="footer">
-<p><a href="http://fuckreader.com/">fuck reader</a> built by <a href="http://cylegage.com/" target="_blank">cyle gage</a> &mdash; powered by <a href="http://cylesoft.com/">cylesoft</a></p>
+<p><a href="https://fuckreader.com/">fuck reader</a> built by <a href="http://cylegage.com/" target="_blank">cyle gage</a> &mdash; powered by <a href="http://cylesoft.com/">cylesoft</a></p>
 </div>
 
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
