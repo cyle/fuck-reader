2c1fddacfdf19db10c26c7500aa37b42148b3d18
Cyle <cylegage@gmail.com>
2013-06-27 20:57:18 -0400

    - feeds with 0 posts are half opacity

--- www/sidebar.php
+++ www/sidebar.php
@@ -16,11 +16,11 @@ if (!isset($users_feeds) || $users_feeds == false || count($users_feeds) == 0) {
 	<li><a href="/feeds/all/"<?php echo ((isset($just_unread) && $just_unread == false && !isset($selected_feed_id)) ? ' class="active"': ''); ?>>Show All Posts, even Read</a></li>
 	<li><a href="/starred/">Show Only Starred Posts</a></li>
 	<li>Your Feeds:</li>
-	<ul>
+	<ul id="list-of-feeds">
 	<?php
 	foreach ($users_feeds as $feed_id => $feed_title) {
 		$unread_count = getFeedUnreadCount($current_user_id, $feed_id);
-		echo '<li>';
+		echo '<li'.(($unread_count == 0) ? ' class="zero-feed"': '').'>';
 		//echo '['.$unread_count.'] <a href="/feed/'.$feed_id.'/" '.((isset($selected_feed_id) && $feed_id == $selected_feed_id) ? 'class="active"': '').'>'.$feed_title.'</a> (<a href="/feed/'.$feed_id.'/all/" '.((isset($selected_feed_id) && $feed_id == $selected_feed_id && isset($just_unread) && $just_unread == false) ? 'class="active"': '').'>All Items</a>)';
 		echo '['.((isset($selected_feed_id) && $feed_id == $selected_feed_id) ? '<span id="unread-feed-count">' : '').$unread_count.((isset($selected_feed_id) && $feed_id == $selected_feed_id) ? '</span>' : '').'] <a href="/feed/'.$feed_id.'/" '.((isset($selected_feed_id) && $feed_id == $selected_feed_id) ? 'class="active"': '').'>'.$feed_title.'</a>';
 		echo '</li>'."\n";
