98b595722a7e962581894c6d78221a2fde0274d6
Cyle <cylegage@gmail.com>
2013-06-29 17:07:40 -0400

    - switched to /dev/urandom for speed, but looking into how to generate more entropy for /dev/random

--- www-includes/login_check.php
+++ www-includes/login_check.php
@@ -11,7 +11,7 @@
 
 #Generate a random key from /dev/random
 function get_key($bit_length = 128){
-	$fp = @fopen('/dev/random','rb');
+	$fp = @fopen('/dev/urandom','rb'); // should be /dev/random but it's too slow
 	if ($fp !== FALSE) {
 		$key = substr(base64_encode(@fread($fp,($bit_length + 7) / 8)), 0, (($bit_length + 5) / 6)  - 2);
 		@fclose($fp);
