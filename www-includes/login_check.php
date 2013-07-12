b2c14761f6c83abce1343350b8155265adc5fb4d
Cyle <cylegage@gmail.com>
2013-07-11 21:45:20 -0400

    - added flood control for logins and registrations

--- www-includes/login_check.php
+++ www-includes/login_check.php
@@ -15,6 +15,8 @@ require_once('account_tools.php');
 $current_user = array();
 $current_user['loggedin'] = false;
 
+$flood_control_script_name = 'login';
+
 if (isset($_COOKIE['fucksession']) && trim($_COOKIE['fucksession']) != '') { // user has a session already?
 	
 	require_once('dbconn_mysql.php');
@@ -62,6 +64,27 @@ if (isset($_COOKIE['fucksession']) && trim($_COOKIE['fucksession']) != '') { // 
 } else if (isset($_POST['e']) && isset($_POST['p'])) { // user is trying to log in?
 	
 	require_once('dbconn_mysql.php');
+	
+	$flood_control_script_name = "'".$mysqli->escape_string($flood_control_script_name)."'";
+	
+	// login flood control
+	$has_flood_control_limit = false;
+	$attempt_ip_db = "'".$mysqli->escape_string(trim($_SERVER['REMOTE_ADDR']))."'";
+	$check_flood_control = $mysqli->query("SELECT * FROM flood_control WHERE script=$flood_control_script_name AND ipaddr=$attempt_ip_db");
+	if ($check_flood_control->num_rows > 0) {
+		$has_flood_control_limit = true;
+		$flood_control_info = $check_flood_control->fetch_assoc();
+		if ($flood_control_info['tsc'] < (time() - 1800)) {
+			// it was over half an hour ago -- remove it
+			$delete_flood_control = $mysqli->query('DELETE FROM flood_control WHERE id='.$flood_control_info['id']);
+			$has_flood_control_limit = false;
+		} else {
+			$update_flood_control = $mysqli->query('UPDATE flood_control SET attempts=attempts+1 WHERE id='.$flood_control_info['id']);
+			if ($flood_control_info['attempts'] > 20) {
+				die('Sorry, you have tried to log in unsuccessfully way too many times. Please try again in a half hour or so.');
+			}
+		}
+	}
 		
 	$users_email_db = "'".$mysqli->escape_string(trim($_POST['e']))."'";
 	
@@ -71,7 +94,15 @@ if (isset($_COOKIE['fucksession']) && trim($_COOKIE['fucksession']) != '') { // 
 		
 		// check password
 		if (crypt(trim($_POST['p']), $current_user_row['pwrdlol']) != $current_user_row['pwrdlol']) {
+			if (!$has_flood_control_limit) {
+				$insert_flood_control = $mysqli->query("INSERT INTO flood_control (ipaddr, attempts, script, tsc) VALUES ($attempt_ip_db, 1, $flood_control_script_name, UNIX_TIMESTAMP())");
+			}
 			die('Your password was incorrect, please try again.');
+		} else {
+			// password checked out, clear flood control, if there was any
+			if ($has_flood_control_limit) {
+				$delete_flood_control = $mysqli->query('DELETE FROM flood_control WHERE id='.$flood_control_info['id']);
+			}
 		}
 		
 		// ok, cool
@@ -106,6 +137,9 @@ if (isset($_COOKIE['fucksession']) && trim($_COOKIE['fucksession']) != '') { // 
 		header('Location: /feeds/');
 		die();
 	} else {
+		if (!$has_flood_control_limit) {
+			$insert_flood_control = $mysqli->query("INSERT INTO flood_control (ipaddr, attempts, script, tsc) VALUES ($attempt_ip_db, 1, $flood_control_script_name, UNIX_TIMESTAMP())");
+		}
 		die('Could not find that email address, sorry. Try again, I guess.');
 	}
 				
--- www/new_user.php
+++ www/new_user.php
@@ -34,11 +34,40 @@ require_once('../www-includes/account_tools.php');
 
 require_once('../www-includes/dbconn_mysql.php');
 
+// check flood control
+$flood_control_script_name = "'".$mysqli->escape_string('register')."'";
+$has_flood_control_limit = false;
+$attempt_ip_db = "'".$mysqli->escape_string(trim($_SERVER['REMOTE_ADDR']))."'";
+$check_flood_control = $mysqli->query("SELECT * FROM flood_control WHERE script=$flood_control_script_name AND ipaddr=$attempt_ip_db");
+if ($check_flood_control->num_rows > 0) {
+	$has_flood_control_limit = true;
+	$flood_control_info = $check_flood_control->fetch_assoc();
+	if ($flood_control_info['tsc'] < (time() - 1800)) {
+		// it was over half an hour ago -- remove it
+		$delete_flood_control = $mysqli->query('DELETE FROM flood_control WHERE id='.$flood_control_info['id']);
+		$has_flood_control_limit = false;
+	} else {
+		$update_flood_control = $mysqli->query('UPDATE flood_control SET attempts=attempts+1 WHERE id='.$flood_control_info['id']);
+		if ($flood_control_info['attempts'] > 20) {
+			die('Sorry, you have tried to register unsuccessfully way too many times. Please try again in a half hour or so.');
+		}
+	}
+}
+
+
 // check invite code...
 $invite_code_db = "'".$mysqli->escape_string(trim($_POST['i']))."'";
 $check_invite_code = $mysqli->query("SELECT * FROM user_invites WHERE invite_code=$invite_code_db AND is_used=0");
 if ($check_invite_code->num_rows == 0) {
+	if (!$has_flood_control_limit) {
+		$insert_flood_control = $mysqli->query("INSERT INTO flood_control (ipaddr, attempts, script, tsc) VALUES ($attempt_ip_db, 1, $flood_control_script_name, UNIX_TIMESTAMP())");
+	}
 	die('sorry, your invite code is already used or otherwise invalid.');
+} else {
+	// invite code checked out, clear flood control, if there was any
+	if ($has_flood_control_limit) {
+		$delete_flood_control = $mysqli->query('DELETE FROM flood_control WHERE id='.$flood_control_info['id']);
+	}
 }
 
 // ok, it's cool, make a new user
