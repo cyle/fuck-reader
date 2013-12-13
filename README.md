db15098047ac415967ecb14db6c80435730c2b00
Cyle Gage <cylegage@gmail.com>
2013-12-12 22:21:40 -0500

    - readying for public release

--- README.md
+++ README.md
@@ -1,22 +1,22 @@
 # FUCK READER
 
-A very simple RSS reader.
+A very simple RSS reader. I built it just before Google Reader died, and I've been using this every day on my own server. A couple of my friends use it, too.
 
 Front-end is PHP, feed fetching is Ruby. Feeds, posts, and user post state all stored in MySQL.
 
-## Front-end setup
+## Front-end installation
 
-This was built with lighttpd and php5-cgi (running via fastcgi). Set document root to the "www" folder.
+This was built with lighttpd and php5-cgi (running via fastcgi). Set document root to the `www` folder.
 
-Edit **www-includes/dbconn_mysql.example.php** and rename it to dbconn_mysql.php when you're done.
+Edit `www-includes/dbconn_mysql.example.php` with the necessary settings and rename it to `dbconn_mysql.php` when you're done.
 
-If you want to use Google Analytics, edit **config/google_analytics.example.php** and rename it to google_analytics.php when you're done.
+If you want to use Google Analytics, edit `config/google_analytics.example.php` and rename it to `google_analytics.php` when you're done. If you **do not** want to use Google Analytics, then comment out the last line in `footer.php` which mentions it.
 
-SQL set up not included yet...
+I used MySQL, and included in `config/fuckreader.sql` are the table schemas used by FUCK READER. Just make a database called "fuckreader" and import that using phpMyAdmin or something.
 
 ## Feed Fetching Prereqs
 
-Feed fetching uses the "feedzirra" library from the folks at feedbin.
+Feed fetching uses the ["feedzirra" library](https://github.com/pauldix/feedzirra) from the folks at feedbin.
 
 On Debian-based servers (Debian, Ubuntu), you need these prereqs:
 
@@ -33,17 +33,25 @@ Install feedzirra:
 
     gem install feedzirra
 
-Now you can use the getfeeds.rb file. Fun!
+Now you can use the `getfeeds.rb` file. Fun!
 
 ## Crontab
-
-Make a logs directory alongside www and www-includes and whatnot:
-
-    mkdir /path/to/fuckreader/logs
     
 Put this in your crontab file:
 
     */5 * * * * cd /path/to/fuckreader/ruby; /usr/bin/ruby getfeeds.rb > ../logs/feeds.log
     0 3 * * * cd /path/to/fuckreader/www-includes; /usr/bin/php cleanup.php > ../logs/cleanup.log
     
-You're free to set it to whatever frequency you want.
+You're free to set it to whatever frequency you want. (See note below about the cleanup script, btw.)
+
+## Notes -- please read
+
+The biggest and most important note I can give is that **this is an unfinished project** and it has quirks. For instance, the cleanup script deletes stuff older than 30 days, but then if there are things older than 30 days in the RSS feeds you're scraping, they'll pop back up. That's annoying. Haven't figured that out yet, it probably means I'll need to cache things forever in some fashion. So for now, *I've turned the cleanup script off on my own server*.
+
+There are a lot of planned features. I have no idea what'll be breaking what in the future. However, since I do use this every day, I probably won't make any features that seriously breaks what's already here.
+
+The `getfeeds.rb` script is not as efficient as I'd like, and with very dense RSS feeds (ones that update very often), it can choke a lot of memory. I plan on rewriting it in a better language, like Go.
+
+There's some Redis functionality that is half-in here, but it doesn't actually do anything, so don't worry about the Redis stuff. It was a failed experiment.
+
+There's a lot of MySQL indexing stuff going on which makes this all a lot faster, but in general if you have 50+ feeds, the site will be kinda slow. Be patient with it.
---
+++ config/fuckreader.sql
@@ -0,0 +1,120 @@
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+
+CREATE TABLE `feeds` (
+  `feed_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `feed_url` text NOT NULL,
+  `feed_title` varchar(250) DEFAULT NULL,
+  `feed_homeurl` varchar(250) DEFAULT NULL,
+  `tsc` int(11) NOT NULL,
+  `tsu` int(11) NOT NULL,
+  PRIMARY KEY (`feed_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+
+CREATE TABLE `flood_control` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ipaddr` varchar(64) NOT NULL,
+  `script` varchar(250) NOT NULL,
+  `attempts` int(11) NOT NULL,
+  `tsc` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `ipaddr` (`ipaddr`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+CREATE TABLE `posts` (
+  `post_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `feed_id` int(11) unsigned NOT NULL,
+  `post_title` varchar(250) DEFAULT NULL,
+  `post_guid` varchar(250) NOT NULL,
+  `post_permalink` text NOT NULL,
+  `post_content` text NOT NULL,
+  `post_byline` varchar(250) DEFAULT NULL,
+  `post_pubdate` int(11) NOT NULL,
+  `ts` int(11) NOT NULL,
+  `chksum` varchar(50) NOT NULL,
+  PRIMARY KEY (`post_id`),
+  KEY `feed_id_index` (`feed_id`),
+  KEY `chksum_index` (`chksum`),
+  KEY `ts_index` (`ts`),
+  KEY `post_title_index` (`post_title`),
+  KEY `post_guid_index` (`post_guid`),
+  KEY `major_index` (`ts`,`feed_id`,`post_id`),
+  KEY `retrieval_index` (`feed_id`,`post_id`),
+  KEY `pubdate_index` (`post_pubdate`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+
+CREATE TABLE `users` (
+  `user_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `uname` varchar(250) DEFAULT NULL,
+  `pwrdlol` varchar(250) NOT NULL,
+  `email` varchar(250) NOT NULL,
+  `tsc` int(11) NOT NULL,
+  `tsu` int(11) NOT NULL,
+  `lastaccess` int(11) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+
+CREATE TABLE `users_feeds` (
+  `row_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) unsigned NOT NULL,
+  `feed_id` int(11) unsigned NOT NULL,
+  `tsc` int(11) NOT NULL,
+  PRIMARY KEY (`row_id`),
+  KEY `user_id_index` (`user_id`),
+  KEY `feed_id_index` (`feed_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+
+CREATE TABLE `users_read_posts` (
+  `row_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) unsigned NOT NULL,
+  `post_id` int(11) unsigned NOT NULL,
+  `tsc` int(11) NOT NULL,
+  PRIMARY KEY (`row_id`),
+  UNIQUE KEY `user_read_key` (`user_id`,`post_id`),
+  KEY `user_id_index` (`user_id`),
+  KEY `post_id_index` (`post_id`),
+  KEY `retrieval_index` (`user_id`,`post_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+
+CREATE TABLE `users_star_posts` (
+  `row_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) unsigned NOT NULL,
+  `post_id` int(11) unsigned NOT NULL,
+  `tsc` int(11) NOT NULL,
+  PRIMARY KEY (`row_id`),
+  UNIQUE KEY `user_star_key` (`user_id`,`post_id`),
+  KEY `user_id_index` (`user_id`),
+  KEY `post_id_index` (`post_id`),
+  KEY `retrieval_index` (`user_id`,`post_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+
+CREATE TABLE `user_invites` (
+  `invite_id` int(11) NOT NULL AUTO_INCREMENT,
+  `invite_code` varchar(255) NOT NULL,
+  `owner_id` int(10) unsigned NOT NULL,
+  `is_used` tinyint(1) NOT NULL DEFAULT '0',
+  `by_new_user` int(11) unsigned DEFAULT NULL,
+  `tsc` int(11) NOT NULL DEFAULT '0',
+  `used_ts` int(11) DEFAULT NULL,
+  PRIMARY KEY (`invite_id`),
+  UNIQUE KEY `invite_code_unique` (`invite_code`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+
+CREATE TABLE `user_sessions` (
+  `session_key` varchar(255) DEFAULT NULL,
+  `session_secret` varchar(255) DEFAULT NULL,
+  `user_id` int(11) unsigned NOT NULL,
+  `expires` int(11) NOT NULL,
+  `ts` int(11) NOT NULL,
+  KEY `session_key_index` (`session_key`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
