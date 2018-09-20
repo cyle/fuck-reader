5ccc4d90eae30493223412dcfe77c1916bacd585
Cyle Gage <cylegage@gmail.com>
2018-09-20 00:00:08 -0400

    fixed encoding issue causing dupes

--- ruby/getfeeds.rb
+++ ruby/getfeeds.rb
@@ -18,6 +18,7 @@ require 'feedjira'
 require 'mysql2'
 require 'yaml'
 require 'digest/sha1'
+require 'unidecoder'
 
 YAML::ENGINE.yamler = 'psych'
 
@@ -57,14 +58,14 @@ dbresults.each do |row|
 end
 
 # now, with each feed URL, grab it and parse it
-feed_urls.each { |feed_info| 
-	
+feed_urls.each { |feed_info|
+
 	puts ""
 	puts "dealing with " + feed_info["feed_url"]
-	
+
 	feed_id = feed_info["feed_id"].to_i
 	feed_new_posts_count = 0
-	
+
 	# get the raw feed
 	feedraw = Feedjira::Feed.fetch_raw("" + feed_info["feed_url"] + "", { :timeout => 5 })
 
@@ -74,7 +75,7 @@ feed_urls.each { |feed_info|
 		puts "error getting feed, returned: " + feedraw.to_s
 		next
 	end
-	
+
 	begin
 		# try parsing the feed!
 		parsed = Feedjira::Feed.parse(feedraw)
@@ -87,13 +88,13 @@ feed_urls.each { |feed_info|
 		# successfully parsed... deal with it!
 		puts ""
 		puts "Feed title: " + parsed.title unless parsed.title.nil?
-				
-		parsed.entries.each do |entry| 
-			
+
+		parsed.entries.each do |entry|
+
 			# okay now we are parsing entries, cool...
-			
+
 			entry_content_hash = "no hash to be made!"
-			
+
 			if entry.content.nil? and entry.summary.nil?
 				# shit -- both are nil? wtf
 				#entry_content_hash = "both summary and content are nil"
@@ -112,38 +113,38 @@ feed_urls.each { |feed_info|
 				# both content and summary are available
 				entry_content_hash = Digest::SHA1.hexdigest entry.content
 			end
-			
+
 			if entry.url.nil?
 				puts "Entry has no URL, moving on..."
 				next
 			end
-			
+
 			entry_guid_db = "'" + (Digest::SHA1.hexdigest entry.url) + "'"
-			
-			puts "Entry title: " + entry.title unless entry.title.nil?
+
+			puts "Entry title: " + entry.title.to_ascii unless entry.title.nil?
 			puts "Entry content hash: " + entry_content_hash
-			
+
 			# check to see if this entry's hash has already been recorded
 			#dbchk_query = "SELECT post_id FROM posts WHERE chksum='"+entry_content_hash+"' AND feed_id=" + feed_info["feed_id"].to_s)
-			
+
 			# try this dbchk query instead:
 			dbchk_query = "SELECT post_id FROM posts WHERE feed_id="+feed_id.to_s+" AND post_guid="+entry_guid_db+" AND "
 			if entry.title.nil?
 				dbchk_query = dbchk_query + "post_title IS NULL"
 			else
-				dbchk_query = dbchk_query + "post_title='" + dbclient.escape(entry.title).slice(0..249) + "'"
+				dbchk_query = dbchk_query + "post_title='" + dbclient.escape(entry.title.to_ascii).slice(0..249) + "'"
 			end
-			
+
 			dbchkresults = dbclient.query(dbchk_query)
 			unless dbchkresults.count > 0
 				# okay, save this entry to the database, since it's new
-				
+
 				if entry.title.nil?
 					entry_title_db = 'null'
 				else
-					entry_title_db = "'" + dbclient.escape(entry.title) + "'"
+					entry_title_db = "'" + dbclient.escape(entry.title.to_ascii) + "'"
 				end
-				
+
 				if entry.content.nil? and entry.summary.nil?
 					entry_content_db = "'" + dbclient.escape("'No content.'") + "'"
 				elsif entry.content.nil? and !entry.summary.nil?
@@ -151,57 +152,57 @@ feed_urls.each { |feed_info|
 				else
 					entry_content_db = "'" + dbclient.escape(entry.content) + "'"
 				end
-				
+
 				if entry.author.nil?
 					entry_byline_db = 'null'
 				else
 					entry_byline_db = "'" + dbclient.escape(entry.author) + "'"
 				end
-				
+
 				entry_link_db = "'" + dbclient.escape(entry.url) + "'"
 				entry_pubdate_db = dbclient.escape(entry.published.to_i.to_s)
 				entry_ts_db = Time.now.to_i.to_s
 				entry_chksum_db = "'" + entry_content_hash + "'"
-				
+
 				dbsaveentry = dbclient.query("INSERT INTO posts (feed_id, post_title, post_guid, post_permalink, post_byline, post_pubdate, ts, chksum) VALUES (#{feed_id}, #{entry_title_db}, #{entry_guid_db}, #{entry_link_db}, #{entry_byline_db}, #{entry_pubdate_db}, #{entry_ts_db}, #{entry_chksum_db})")
-				
+
 				new_post_id = dbclient.last_id
-				
+
 				dbsavecontent = dbclient.query("INSERT INTO posts_content (post_id, post_content) VALUES (#{new_post_id}, #{entry_content_db})")
-				
+
 				puts "entry added to database"
-				
+
 				entries_new += 1
 				feed_new_posts_count += 1
-				
+
 			else
 				puts "entry already in the database, moving on"
 			end
-			
+
 			entries_processed += 1
-			
+
 		end
-		
+
 		# try updating feed title and whatnot
 		unless parsed.title.nil?
 			feed_title_db = "'"+dbclient.escape(parsed.title)+"'"
 		else
 			feed_title_db = 'null'
 		end
-		
+
 		unless parsed.url.nil?
 			feed_homeurl_db = "'"+dbclient.escape(parsed.url)+"'"
 		else
 			feed_homeurl_db = 'null'
 		end
-		
+
 		# update the feed row
 		updatefeedrow = dbclient.query("UPDATE feeds SET tsu=UNIX_TIMESTAMP(), feed_title=#{feed_title_db}, feed_homeurl=#{feed_homeurl_db} WHERE feed_id=#{feed_id}")
-		
+
 		# all done with this feed, moving on
 		feeds_processed += 1
 	end
-	
+
 }
 
 # okay, all done
